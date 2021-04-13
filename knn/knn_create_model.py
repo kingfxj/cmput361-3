59c9b0c48e4eddeb91caf49ede92c9ae0d8b1c8f
ScottKavalinas <36358126+scottkavalinas@users.noreply.github.com>
2021-04-12 21:38:19 -0600

    Update knn_create_model.py

---
+++ knn/knn_create_model.py
@@ -0,0 +1,19 @@
+#take train corpus --> output to CSV
+
+class KnnModel:
+    def __init__(self,corpus):
+        self.corpus = corpus
+        self.IdfLines = []
+        self.VectorLines = []
+    
+    def vectorize(self):
+        for document in self.corpus:
+            #represent as a vector using ltn wieght scheme
+         
+    def getIdf(self):
+        for trerm in document:
+            file.write("IDF", Term,, IDFWeight)
+
+    def getVector(self):
+        #for class in model:
+        file.write("Vectpr", class, "STRING REPRESENTATION OF VECTOR FOR CLASS')")
